4c4f0dd43ec563c4e864a2f2438da8c856acaed0
Dreadpipe <benharris83@gmail.com>
2019-03-02 09:53:21 -0600

    Added starting pseudo-code

---
+++ assets/js/app.js
@@ -0,0 +1,27 @@
+/*
+Document ready goes here
+
+I need an array that stores strings of "things I like"
+
+I need a for loop that cycles through my array
+
+I need to append the array to buttons
+
+I need an on click event for the buttons
+
+I need the event to access the Giphy API
+
+I need to append the data returned as gifs to the HTML
+
+I need the gifs to appear in a maximum of 10 results
+
+I need the gifs to REPLACE each other as the button is clicked
+
+I need the gifs to respond to clicks, in that they animate or still
+
+I need the form/submit button to receive input
+
+I need the input to be appended to a new button in the row of buttons
+
+
+*/
