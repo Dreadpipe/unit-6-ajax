1a727462a76934f82392df8083f55c9f78f87408
Dreadpipe <benharris83@gmail.com>
2019-03-03 12:55:38 -0600

    added jQuery- still/animate, queryURL, etc

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,61 @@
-/*
-Document ready goes here
+// GIPHY WARRIORS!
+// Document ready goes here
+// CHANGE ALL VARS TO LETS/CONST
+$(document).ready(function() {
+// STEP ONE: I need an array that stores strings of "things I like"
 
-I need an array that stores strings of "things I like"
+// I need a for loop that cycles through my array
 
-I need a for loop that cycles through my array
+// I need to append the array to buttons
 
-I need to append the array to buttons
+// STEP TWO: I need an on click event for the buttons
+$("#hero-button").on("click", function() {
 
-I need an on click event for the buttons
+    // this is the API URL, stored in a variable, which we'll use to call this API later
+    // must be SPECIFICALLY catered to your needs
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + hero + "&api_key=e5k3DBOsJmADSoZFSu9EOG0ML89Tz2m3&limit=10";
+    var hero =  $(this).attr("data-hero");
 
-I need the event to access the Giphy API
+    // this is the ajax function that is getting data from the source
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+// I need the event to access the Giphy API
+        .then(function(response) {
 
-I need to append the data returned as gifs to the HTML
+            var imageUrl = response.data.image_original_url;
 
-I need the gifs to appear in a maximum of 10 results
+            var heroImage = $("<img>");
+// I need to append the data returned as gifs to the HTML
+// I need the gifs to appear in a maximum of 10 results (alter queryURL)
+            heroImage.attr("src", imageUrl);
+            heroImage.attr("alt", "hero image");
+            // adding class to effect all images
+            heroImage.attr("class", ".gif")
 
-I need the gifs to REPLACE each other as the button is clicked
+            $("#images").prepend(heroImage);
+        });
+    //ends the on-click event
+    });
+// I need the gifs to REPLACE each other as the button is clicked
 
-I need the gifs to respond to clicks, in that they animate or still
+// I need the gifs to respond to clicks, in that they animate or still
+    $(".gif").on("click", function() {
+        var state = $(this).attr("data-state");
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        }
+        else {
 
-I need the form/submit button to receive input
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+    });
+// I need the form/submit button to receive input
 
-I need the input to be appended to a new button in the row of buttons
+// I need the input to be appended to a new button in the row of buttons
 
-
-*/
+// This ends my script/document ready function.
+});
