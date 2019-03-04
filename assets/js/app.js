317a3596128f9f93535e3ab31025d11884961892
Dreadpipe <benharris83@gmail.com>
2019-03-03 19:06:08 -0600

    Thanks Leona's!

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,6 +8,8 @@ $(document).ready(function() {
         "King Arthur",
         "Joan of Arc",
         "Black Knight",
+        "Dragoon",
+        "Knight Errant"
     ]
     // This function creates buttons
     function renderButtons() {
@@ -16,23 +18,28 @@ $(document).ready(function() {
         // I need a for loop that cycles through my array
     for (var i = 0; i < topics.length; i++) {
         var a = $("<button>");
-        //added .topic class for later functionality 
-        a.addClass("topic");
+        // added .topic class for later functionality 
+        // I feel like all of this could be optimized
+        a.attr("type", "button");
+        a.addClass("topic btn btn-dark");
         a.attr("data-topic", topics[i]);
+        // adds bootstrap types 
         a.text(topics[i]);
         $("#buttons").append(a);
     }
     }
-
+    
 
 
 // STEP TWO: I need an on click event for the buttons
-$("#buttons").on("click", function() {
-
+$(document).on("click", ".topic", function() {
+    
     // this is the API URL, stored in a variable, which we'll use to call this API later
     // must be SPECIFICALLY catered to your needs
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=e5k3DBOsJmADSoZFSu9EOG0ML89Tz2m3&q=" + topic + "&limit=10";
     var topic =  $(this).attr("data-topic");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=e5k3DBOsJmADSoZFSu9EOG0ML89Tz2m3&q=" + topic + "&limit=10&offset=4&lang=en";
+    console.log(queryURL)
+    
 
     // this is the ajax function that is getting data from the source
     $.ajax({
@@ -45,7 +52,7 @@ $("#buttons").on("click", function() {
             var results = response.data; 
             
             for (var i = 0; i < results.length; i++) {
-                console.log(results[i].images.fixed_height.url)
+                //console.log(results[i].images.fixed_height.url)
             
                 
                 var gifDiv = $("<div>").attr("id", "gifs");
@@ -54,11 +61,16 @@ $("#buttons").on("click", function() {
                 topicImage.attr("data-animate", results[i].images.fixed_height.url);
                 topicImage.attr("data-state", "still");
                 topicImage.attr("class", "gif");
+                
 
                 $("#images").prepend(gifDiv);
                 gifDiv.append(topicImage);
+                var rating = results[i].rating;
+                    pRating = $("<p>").text("Rated: " + rating);
+                    console.log(rating);
                 //need to add ratings
-                //need to add titles
+                gifDiv.append(pRating);
+
                 //use p tags?
             }
 
@@ -68,7 +80,7 @@ $("#buttons").on("click", function() {
 // I need the gifs to REPLACE each other as the button is clicked
 
 // STEP THREE: I need the gifs to respond to clicks, in that they animate or still
-    $(".gif").on("click", function() {
+    $(document).on("click", ".gif", function() {
         var state = $(this).attr("data-state");
         if (state === "still") {
             $(this).attr("src", $(this).attr("data-animate"));
@@ -81,6 +93,12 @@ $("#buttons").on("click", function() {
         }
     });
 // I need the form/submit button to receive input
+// use id "#add-topic" and "topic-input"
+    $("#add-topic").on("click", function(event) {
+        event.preventDefault();
+
+        var newTopic = $("#topic-input")
+    })
 
 // I need the input to be appended to a new button in the row of buttons
 // I need to call my button function both in and out of function
